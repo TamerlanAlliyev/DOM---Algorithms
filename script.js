bfeff4a3fd9c0f8b055cf24fe77c232977e226ab
TamerlanAlliyev <tamerlanma@code.edu.az>
2023-12-18 14:31:03 +0400

    15

--- script.js
+++ script.js
@@ -51,10 +51,10 @@ for (let i = 0; i <= 100; i++) {
     if(i%3===0){
         console.log("Fizz")
     }
-    else if (i%5===0){
+    else if (i%15===0){
         console.log("Buzz")
     }
-    else if(i%15===0){
+    else if(i%5===0){
         console.log("FizzBuzz")
     }else{
         console.log(i)
